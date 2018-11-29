8cb884990251994ca1267ba305e5c8310a4d6eda
ang1106 <ang1106@naver.com>
2018-11-29 19:57:54 +0900

    필요없는 부분 삭제
    2차 수정

--- javascript/qrcodegen-demo.js
+++ javascript/qrcodegen-demo.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 var app = new function () {
 
 	function initialize() {
@@ -9,9 +8,6 @@ var app = new function () {
 			if (elems[i].id.indexOf("version-") != 0)
 				elems[i].oninput = redrawQrCode;
 		}
-		elems = document.querySelectorAll("input[type=radio], input[type=checkbox]");
-		for (var i = 0; i < elems.length; i++)
-			elems[i].onchange = redrawQrCode;
 		redrawQrCode();
 	}
 
@@ -28,11 +24,7 @@ var app = new function () {
 		var ecl = qrcodegen.QrCode.Ecc.LOW
 		var text = document.getElementById("text-input").value;
 		var segs = qrcodegen.QrSegment.makeSegments(text);
-		var minVer = 1;
-		var maxVer = 40;
-		var mask = -1;
-		var boostEcc = 1;
-		var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, minVer, maxVer, mask, boostEcc);
+		var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, 1, 40, -1, 1);
 
 		// Draw image output
 		qr.drawCanvas(8, 4, canvas);
@@ -54,23 +46,6 @@ var app = new function () {
 			} else
 				return "multiple";
 		}
-	}
-
-
-	this.handleVersionMinMax = function (which) {
-		var minElem = document.getElementById("version-min-input");
-		var maxElem = document.getElementById("version-max-input");
-		var minVal = 1;
-		var maxVal = 40;
-		minVal = Math.max(Math.min(minVal, qrcodegen.QrCode.MAX_VERSION), qrcodegen.QrCode.MIN_VERSION);
-		maxVal = Math.max(Math.min(maxVal, qrcodegen.QrCode.MAX_VERSION), qrcodegen.QrCode.MIN_VERSION);
-		if (which == "min" && minVal > maxVal)
-			maxVal = minVal;
-		else if (which == "max" && maxVal < minVal)
-			minVal = maxVal;
-		minElem.value = minVal.toString();
-		maxElem.value = maxVal.toString();
-		redrawQrCode();
 	}
 
 
